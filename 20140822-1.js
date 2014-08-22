85526c328aa9e95cc2f4705201a0deeb1bd3cd5f
Grégoire Charvet 黑瓜 <gregoire@digbil.com>
2014-08-22 15:35:48 +0800

    Remove some stuff

--- 20140822-1.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-app.get('/ping', function(req, res){
-  res.send('Ashley Test');
-});
-var server = app.listen(8000, function() {
-    console.log('clone 123');
-});
