bd734f6e915fac0db8259bcf95e9235125ec7d82
Ashley Chang <ashley.chang@digbil.com>
2014-08-22 15:26:55 +0800

    20140822 commit

---
+++ 20140822-2.js
@@ -0,0 +1,17 @@
+http://expressjs.com/guide.html
+
+http://www.git-scm.com/book/en/Getting-Started
+
+https://www.guerrillamail.com/inbox
+
+https://fusionstage.digbil.com/media?type=image
+
+http://www.git-scm.com/book/en/Getting-Started-About-Version-Control
+
+How about fusionstage... 
+1.System 
+	1.When I change the login user, I need reflash the page. 
+	2. The role is read only, the button of upload new media should disable.
+
+2.User 
+	1.Why creater can not change the role in user/edit page? 
